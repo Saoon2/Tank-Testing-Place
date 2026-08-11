123001ea3a3353b5e411b69810e9873f0f0a4e99
Saoon2 <Pariskaled@gmail.com>
2026-08-11 16:03:41 -0400

    Spriteframes

--- Scripts/laser.gd
+++ Scripts/laser.gd
@@ -12,6 +12,7 @@ func _physics_process(delta):
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("mobs"):
+		
 		#explosion animaton ? Instantiate explosition
 		body.queue_free()
 	
